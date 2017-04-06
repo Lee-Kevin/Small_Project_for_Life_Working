c183d703a355ebaca565a0b133a5f7911c34d2ce
Jiankai Li <lidreamer@foxmail.com>
2017-04-06 09:42:30 +0800

    Update account.py

--- 20161109/script/account.py
+++ 20161109/script/account.py
@@ -2,7 +2,9 @@ import requests
 import json
 
 url = "http://192.168.1.81:9000/k3cloud/services/k3STUservice.asmx/JudgeSpecialUserOnline"
-
-response = requests.request("GET", url, data="", headers="")
-data     = json.loads(response.text)
-print(data['Result'])
+try:
+    response = requests.request("GET", url, data="", headers="")
+    data     = json.loads(response.text)
+    print(data['Result'])
+except Exception,e:
+    print(2)
